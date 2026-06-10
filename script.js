f5e0382f41a8aaf2842ed1dcaaaf2ad1be5375f8
“Michelle <michelle.simms@nic.bc.ca>
2026-06-10 13:31:14 -0400

    Update to script.js to match course files

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
-// console.log("My script is running!");
-// Get the HTML content of the H1
-console.log( $('h1').html() );
+// script.js
+$(document).ready(function() {
+    console.log('Script loaded!');
 
-// Set the HTML content of the H1
-$('h1').html("<em>Interactive</em> Project!"); // Adds emphasis tag too
+    // Change heading HTML
+    $('h1').html("jQuery Makes It <em>Easier</em>!");
 
-// Set the HTML content of the output area
-$('#outputArea').html("<p>Content set by jQuery!</p>");
+    // Add initial text to output area
+    $('#outputArea').text("Ready for clicks...");
 
-// Get text content
-console.log( $('h1').text() ); // Gets "Interactive Project!"
+    // Style the main button with jQuery
+    $('#myButton').css({
+        'background-color': 'cornflowerblue',
+        'color': 'white',
+        'padding': '10px 15px',
+        'border': 'none',
+        'border-radius': '5px',
+        'cursor': 'pointer',
+        'font-size': '1em'
+    });
 
-// Set text content
-$('#outputArea').text("Just plain text here.");
+    // Add the .highlight class to the first paragraph
+    $('p:first').addClass('highlight');
 
-$('#outputArea').append("<span> More text appended.</span>");
+    // Event handler for the main button click
+    $('#myButton').on('click', function() {
+        console.log("Button was clicked!");
 
-// Get a CSS property
-console.log( $('h1').css('color') );
+        // CHALLENGE 2: Input Interaction
+        // Read the current value from #textInput and display it in #outputArea
+        const inputValue = $('#textInput').val();
+        $('#outputArea').text(inputValue);
+        // END CHALLENGE 2
 
-// Set a single CSS property
-$('h1').css('color', 'darkred');
+        $('p:first').toggle();
+        $('h1').toggleClass('active');
 
-// Set multiple CSS properties using an object
-$('#myButton').css({
-  'background-color': 'lightblue',
-  'padding': '10px 15px',
-  'border': 'none',
-  'border-radius': '5px', // Note: CSS property names with hyphens are quoted
-  'cursor': 'pointer'     // Make the mouse cursor a pointer
-});
+        // CHALLENGE 3: CSS Class Toggling
+        // Toggle the .fancy class on #outputArea each time the button is clicked
+        $('#outputArea').toggleClass('fancy');
+        // END CHALLENGE 3
+    });
 
+    // Add a hover effect to the main button
+    $('#myButton').hover(
+        function() { $(this).css('background-color', 'darkblue'); },
+        function() { $(this).css('background-color', 'cornflowerblue'); }
+    );
 
-$("#myButton").on('click', function() {
-    console.log('clicked!');
-    $('#outputArea').html("<strong>Button clicked!</strong> Timestamp: " + Date.now());
-}
-)
+    // CHALLENGE 1: Add More Interaction
+    // Style the hide button to match the main button
+    $('#hideButton').css({
+        'background-color': 'slategray',
+        'color': 'white',
+        'padding': '10px 15px',
+        'border': 'none',
+        'border-radius': '5px',
+        'cursor': 'pointer',
+        'font-size': '1em',
+        'margin-right': '8px'
+    });
+
+    // Click handler: hide the <h1> element when #hideButton is clicked
+    $('#hideButton').on('click', function() {
+        $('h1').hide();
+    });
+    // END CHALLENGE 1
+
+}); // End of $(document).ready()
